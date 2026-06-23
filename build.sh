685f4f069ba16ee92f12794281fd8f396820dc6e
SHP <cs2139@apps.ntpc.edu.tw>
2026-06-23 20:12:06 +0800

    feat(data): 更新反饋資料及活動面試資訊
    - 新增反饋資料，包括名稱、標題、訊息及日期。
    - 更新活動面試資訊，修改面試時間及報名方式。
    - 刪除不必要的伺服器二進位檔案以簡化構建流程。

--- build.sh
+++ build.sh
@@ -11,10 +11,4 @@ GOOS=linux GOARCH=arm64 go build -o bin/server-linux-arm64 . && echo -e "\033[32
 GOOS=darwin GOARCH=arm64 go build -o bin/server-darwin-arm64 . && echo -e "\033[32mServer built successfully for macOS ARM64\033[0m" || echo -e "\033[31mFailed to build server for macOS ARM64\033[0m"
 GOOS=windows GOARCH=arm64 go build -o bin/server-windows-arm64.exe . && echo -e "\033[32mServer built successfully for Windows ARM64\033[0m" || echo -e "\033[31mFailed to build server for Windows ARM64\033[0m"
 
-GOOS=linux GOARCH=arm go build -o bin/server-linux-arm . && echo -e "\033[32mServer built successfully for Linux ARM\033[0m" || echo -e "\033[31mFailed to build server for Linux ARM\033[0m"
-GOOS=windows GOARCH=386 go build -o bin/server-windows-386.exe . && echo -e "\033[32mServer built successfully for Windows 386\033[0m" || echo -e "\033[31mFailed to build server for Windows 386\033[0m"
-
-GOOS=ios GOARCH=arm64 CGO_ENABLED=1 go build -o bin/server-ios-arm64 . && echo -e "\033[32mServer built successfully for iOS ARM64\033[0m" || echo -e "\033[31mFailed to build server for iOS ARM64\033[0m"
-GOOS=android GOARCH=arm64 go build -o bin/server-android-arm64 . && echo -e "\033[32mServer built successfully for Android ARM64\033[0m" || echo -e "\033[31mFailed to build server for Android ARM64\033[0m"
-
 echo "Build process completed."
